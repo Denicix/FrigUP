1668a47309b8946265881e4233e875d9f8bde3d7
Valerio <Valerio>
2017-01-25 11:31:19 +0100

    aggiunto il MainActivity

---
+++ app/src/main/java/com/example/skyli/frigup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.skyli.frigup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by skyli on 25/01/2017.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
