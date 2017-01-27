ec47ddbba6febce5230176f58ed5fa4e9690faad
Danilo Crispi <Danilo Crispi>
2017-01-27 13:19:07 +0100

    inserimento adapter

--- app/src/main/java/com/example/skyli/frigup/VisualizzaProdottiActivity.java
+++ app/src/main/java/com/example/skyli/frigup/VisualizzaProdottiActivity.java
@@ -3,22 +3,32 @@ package com.example.skyli.frigup;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class VisualizzaProdottiActivity extends Activity {
 
     TextView nameTv;
     Intent intent;
-    //String username;
+    RecyclerView productRecyclerView;
+    LinearLayoutManager layoutManager;
+    ProdottiAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizza_prodotti);
         nameTv = (TextView)findViewById(R.id.name_tv);
-      /* intent = getIntent();
-        username = intent.getStringExtra("username");
-        nameTv.setText(username);
-        */
+        intent = getIntent();
+
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new ProdottiAdapter();
+
+        productRecyclerView.setLayoutManager(layoutManager);
+        productRecyclerView.setLayoutManager(adapter);
+
     }
 }
