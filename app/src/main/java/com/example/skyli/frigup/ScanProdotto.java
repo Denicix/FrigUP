e0a32bf0941c605b65f668aee21ad479acb8a9fd
Valerio <Valerio>
2017-01-27 09:48:51 +0100

    aggiunto scan_prodotto

---
+++ app/src/main/java/com/example/skyli/frigup/ScanProdotto.java
@@ -0,0 +1,92 @@
+package com.example.skyli.frigup;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.android.gms.vision.CameraSource;
+
+import java.io.IOException;
+
+/**
+ * Created by skyli on 26/01/2017.
+ */
+
+
+
+public class ScanProdotto extends Activity {
+
+    TextView barcodeInfo;
+    SurfaceView cameraView;
+    CameraSource cameraSource;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_prodotto);
+
+
+        cameraView = (SurfaceView) findViewById(R.id.camera_view);
+        barcodeInfo = (TextView) findViewById(R.id.txtContent);
+
+
+        BarcodeDetector barcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.CODE_128)//QR_CODE)
+                        .build();
+
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+}
